f302d4c877b021a20b5a9306d098812169c5188d
Chandan Kheto <chandankheto46@gmail.com>
2025-02-01 16:22:21 +0530

    Add files via upload

---
+++ Simple example/hello.js
@@ -0,0 +1,12 @@
+
+let scores = [90, 80, 70]; 
+ for (let score of scores) {   
+     console.log(score); 
+}
+  let role = "admin";
+    switch (role) {   
+    case "admin":         
+    console.log("Welcome, Admin!");      
+       break;     
+  } 
+
